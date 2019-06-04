264105881a913b44d174221aaa8a89131990e560
thomas0927 <thomas0927@163.com>
2019-06-04 11:12:14 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
@@ -2,7 +2,9 @@ package com.coding.dojo.wycash;
 
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class MoneyTest {
   @Test
@@ -10,5 +12,14 @@ public class MoneyTest {
     Money five = Money.dollar(5);
     assertEquals(new Dollar(10), five.times(2));
     assertEquals(new Dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+    assertFalse(new Franc(5).equals(Money.dollar(5)));
   }
 }
