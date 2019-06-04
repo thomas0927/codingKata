12a5f63dcfbafbe0c099cbfd0f012acba6aeebc3
thomas0927 <thomas0927@163.com>
2019-06-04 09:38:39 +0800

    Default Changelist

---
+++ codingdojo3/src/main/java/com/coding/dojo/wycash/Franc.java
@@ -0,0 +1,19 @@
+package com.coding.dojo.wycash;
+
+public class Franc {
+  private int amount;
+
+  public Franc(int amount) {
+    this.amount = amount;
+  }
+
+  Franc times(int multiplier) {
+    return new Franc(amount * multiplier);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    Franc franc = (Franc) obj;
+    return amount == franc.amount;
+  }
+}
