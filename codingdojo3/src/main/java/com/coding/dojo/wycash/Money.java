33bee68bb4092389af330d08e1803140ec960836
thomas0927 <thomas0927@163.com>
2019-06-04 11:13:42 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/wycash/Money.java
+++ codingdojo3/src/main/java/com/coding/dojo/wycash/Money.java
@@ -7,6 +7,10 @@ public abstract class Money {
     return new Dollar(amount);
   }
 
+  public static Money franc(int amount) {
+    return new Franc(amount);
+  }
+
   @Override
   public boolean equals(Object obj) {
     Money money = (Money) obj;
--- codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
@@ -18,7 +18,7 @@ public class MoneyTest {
   public void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertTrue(Money.franc(5).equals(new Franc(5)));
     assertFalse(new Franc(5).equals(new Franc(6)));
     assertFalse(new Franc(5).equals(Money.dollar(5)));
   }
