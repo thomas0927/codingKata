54907bebe7cad3229a7a699264adab89b43f2ffd
thomas0927 <thomas0927@163.com>
2019-06-04 15:56:07 +0800

    Default Changelist

--- codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
@@ -101,6 +101,17 @@ public class MoneyTest {
   }
 
   @Test
+  public void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
+  }
+
+  @Test
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
   }
