f13e136820b6dc5cdcf632b99342974ef554c995
thomas0927 <thomas0927@163.com>
2019-06-04 13:54:11 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/wycash/Money.java
+++ codingdojo3/src/main/java/com/coding/dojo/wycash/Money.java
@@ -1,6 +1,6 @@
 package com.coding.dojo.wycash;
 
-public class Money {
+public class Money implements Expression {
   int amount;
   String currency;
 
@@ -36,7 +36,7 @@ public class Money {
     return amount + " " + currency;
   }
 
-  public Money plus(Money addend) {
+  public Expression plus(Money addend) {
     return new Money(amount + addend.amount, currency);
   }
 }
