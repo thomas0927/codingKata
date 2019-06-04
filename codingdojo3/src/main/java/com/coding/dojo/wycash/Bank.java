0a165a176d96a625c71cf61d8daca25ab9f185cf
thomas0927 <thomas0927@163.com>
2019-06-04 15:40:09 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/wycash/Bank.java
+++ codingdojo3/src/main/java/com/coding/dojo/wycash/Bank.java
@@ -12,12 +12,12 @@ public class Bank {
 
   int rate(String from, String to) {
     if (from.equals(to)) {
-        return 1;
+      return 1;
     }
     return rates.get(new Pair(from, to));
   }
 
   public void addRate(String from, String to, int rate) {
-    rates.put(new Pair(from, to), new Integer(rate));
+    rates.put(new Pair(from, to), rate);
   }
 }
--- codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
+++ codingdojo3/src/test/java/com/coding/dojo/wycash/MoneyTest.java
@@ -90,6 +90,17 @@ public class MoneyTest {
   }
 
   @Test
+  public void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(15), result);
+  }
+
+  @Test
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
   }
