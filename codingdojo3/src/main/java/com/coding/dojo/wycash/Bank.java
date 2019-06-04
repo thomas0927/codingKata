f4fb6857bbd4fe90d5f3ba098817c18dab98f67f
thomas0927 <thomas0927@163.com>
2019-06-04 14:44:37 +0800

    Default Changelist

--- codingdojo3/src/main/java/com/coding/dojo/wycash/Bank.java
+++ codingdojo3/src/main/java/com/coding/dojo/wycash/Bank.java
@@ -2,7 +2,7 @@ package com.coding.dojo.wycash;
 
 public class Bank {
   public Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
 
   public void addRate(String from, String to, int rate) {}
